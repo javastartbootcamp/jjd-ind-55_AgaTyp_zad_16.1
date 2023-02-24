e49325584e1f1baf005148dd6b8dd8874eb531d9
Agnieszka Typiak <agnieszka.typiak@mailbox.org>
2023-02-24 10:00:45 +0100

    Rozwiazanie

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -1,5 +1,43 @@
 package pl.javastart.task;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
+
 public enum Season {
-    //uzupełnij
+    SPRING("Wiosna", new String[]{"marzec", "kwiecień", "maj"}),
+    SUMMER("Lato", new String[]{"czerwiec", "lipiec", "sierpień"}),
+    AUTUMN("Jesień", new String[]{"wrzesień", "październik", "listopad"}),
+    WINTER("Zima", new String[]{"grudzień", "styczeń", "luty"});
+
+    private final String description;
+    private final String[] months;
+
+    Season(String description, String[] months) {
+        this.description = description;
+        this.months = months;
+    }
+
+    public String[] getMonths() {
+        return months;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static Season fromDescription(String description) {
+        Season[] values = values();
+        for (Season season : values) {
+            if (season.getDescription().equalsIgnoreCase(description)) {
+                return season;
+            }
+        }
+        return  null;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(months);
+    }
 }
--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -6,7 +6,22 @@ public class SeasonManager {
 
     // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
+        Season season = readSeason(scanner);
+        printMonths(season);
 
+    }
+
+    private void printMonths(Season season) {
+        System.out.println(season);
+    }
+
+    private Season readSeason(Scanner scanner) {
+        System.out.println("Podaj porę roku:");
+        for (Season season : Season.values()) {
+            System.out.println(season.getDescription());
+        }
+        String pickedSeasonDesc = scanner.nextLine();
+        return Season.fromDescription(pickedSeasonDesc);
     }
 
 }
